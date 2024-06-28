b359d39934c621672084153c6df2b2185f5dcdc4
Jonathan Mendoza <john.2114@hotmail.com>
2024-06-28 16:08:22 +0200

    New excersice, for a  new calculator , review this code and study it again.

---
+++ MiniProjectCalculateAverage/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace MiniProjectCalculateAverage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AverageCalculator(); 
+        }
+        public static void AverageCalculator()
+        {
+            // REPASAR CONTENIDO
+            int currentScore;
+            int sum = 0;
+            int counter = 0;
+
+            do
+            {
+                Console.WriteLine("Enter your students score . Enter -1  t0 finish!");
+                currentScore = int.Parse(Console.ReadLine());
+                
+                if(currentScore != -1)
+                {
+                    sum = sum + currentScore;
+                    counter++;
+
+                }
+            } while (currentScore != -1);
+
+            int avareage = sum / counter;
+            Console.WriteLine("the avarage is: " + avareage);
+            Console.ReadKey();
+           
+
+
+
+
+        }
+
+
+
+
+    }
+}

945834940f019f5b345925e6d61d83017f1541a1
Jonathan Mendoza <john.2114@hotmail.com>
2024-06-28 16:38:17 +0200

    IntroductionToForLoops

---
+++ IntroductionsTo Arrays/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace IntroductionsTo_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ArraysWithInt();
+            StringsDay();
+
+        }
+
+
+
+        public static void ArraysWithInt()
+        {
+
+            //declare an array
+            /*
+            int[] myIntArray = new int[5];
+
+            Assigned values to the array
+            myIntArray[0] = 5;
+            myIntArray[1] = 12;
+            myIntArray[2] = 13;
+            myIntArray[3] = 14;
+            myIntArray[4] = 15;
+
+
+            Console.WriteLine(myIntArray[0]);
+            */
+
+            int[] myIntArray = [5, 12, 13, 14, 15];
+
+            Console.WriteLine(myIntArray[1]);
+
+        }
+
+
+        public static void StringsDay()
+        {
+            string[] days = ["Mon", "Tue", "Wen", "Thurs", "Fri"];
+            Console.WriteLine("Lenght = " + days.Length);
+
+
+          /*  for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine(days[i]);
+            }
+          */
+
+            foreach (var item in days)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}

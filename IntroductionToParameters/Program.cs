89bda4a7b7f7e0fe9d15fc5cf3393c711aef3d82
Jonathan Mendoza <john.2114@hotmail.com>
2024-06-30 19:41:26 +0200

    Introduction to Parameters and Arguments

---
+++ IntroductionToParameters/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+//A method that has the parameter myString of Type String
+//Method declaration
+
+
+//Parameter 
+void WriteSomethingSpecific(string myString)
+{
+
+    Console.WriteLine("You passed this argument to me:  " + myString);
+    Console.ReadKey();
+
+
+}
+
+string username = "frank"; 
+//argument
+//calling the method using an Argument calles "myUSERname"
+WriteSomethingSpecific(username);
+*/
+
+using System.Collections.Concurrent;
+
+int AdTwoValues(int value1, int value2)
+{
+
+    int result = value1 + value2;
+
+    Console.WriteLine("result: " + result);
+    return result;
+}
+
+Console.WriteLine("Add two numbers: " );
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+
+AdTwoValues(num1, num2);
+
+

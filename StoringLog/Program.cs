da3aa9ed3ec868c27e38a40a9eddc12237a248fb
Jonathan Mendoza <john.2114@hotmail.com>
2024-08-12 21:15:05 +0200

    how to create txt with c#

---
+++ StoringLog/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace StoringLog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\Logs";
+            string filePath = System.IO.Path.Combine(directoryPath, "log.txt");
+            string message = "this is a log entry";
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+
+            }
+
+
+            File.AppendAllText(filePath, "Hello Wold" + "\n");
+            Console.ReadKey();
+
+        }
+    }
+}

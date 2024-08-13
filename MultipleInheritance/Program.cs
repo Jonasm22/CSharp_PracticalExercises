df89b74e86552973eb957f3c8fc31189986484ab
Jonathan Mendoza <john.2114@hotmail.com>
2024-08-13 14:14:20 +0200

    Multiple inheritance Introduction

---
+++ MultipleInheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace MultipleInheritance
+{
+
+   public interface IPrintable
+    {
+        void Print();
+    }
+
+    public  interface IScannable
+    {
+        void Scan();
+    }
+    
+    public class MultipleFunctionPrinter : IPrintable , IScannable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printing Document");
+        }
+
+        public void Scan()
+        {
+            Console.WriteLine("Scanning Document");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MultipleFunctionPrinter printer = new MultipleFunctionPrinter();    
+            printer.Print();
+            printer.Scan();
+        }
+    }
+}

f659b0fedd33406056e77cfd541056bb329781cc
Jonathan Mendoza <john.2114@hotmail.com>
2024-08-13 13:58:42 +0200

    Three Diferemtes Ways to Independency Injection

---
+++ DependencyInjectionLecture/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace DependencyInjectionLecture
+{
+    public class Hammer
+    {
+        public void Use()
+        {
+            Console.WriteLine("Hammering Nails!");
+        }
+     
+    }
+
+    public class Saw
+    {
+        public void Use()
+        {
+            Console.WriteLine("Sawing wood");
+        }
+    }
+
+    public class Builder
+    {
+        private Hammer _hammer;
+        private Saw _saw;
+
+        /*
+        public Builder()
+        {
+            _hammer = new Hammer(); // Builder os responsible for creating its dependency
+            _saw = new Saw();   
+        }
+        */
+
+        // Constructor  Dependency Injection (DI)
+        public Builder(Hammer hammer, Saw saw)
+        {
+            _hammer = hammer;
+            _saw = saw;
+
+        }
+
+
+        public void BuilHouse()
+        {
+            _hammer.Use();
+            _saw.Use();
+            Console.WriteLine("House Builded");
+        }
+    }
+
+    
+        
+        
+      
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hammer hammer = new Hammer();
+            Saw saw = new Saw();
+            Builder builder = new Builder(hammer, saw);
+            builder.BuilHouse();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ DependendyInjectionUsingSetter/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace DependendyInjectionUsingSetter
+{
+    public class Hammer
+    {
+        public void Use()
+        {
+            Console.WriteLine("Hammering Nails!");
+        }
+
+    }
+
+    public class Saw
+    {
+        public void Use()
+        {
+            Console.WriteLine("Sawing wood");
+        }
+    }
+
+    public class Builder
+    {
+        public Hammer Hammer {  get; set; }
+        public Saw Saw { get; set; }
+
+        public void BuilHouse()
+        {
+            Hammer.Use();
+            Saw.Use();
+            Console.WriteLine("House Built");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hammer hammer = new Hammer();
+            Saw saw = new Saw();
+            Builder builder = new Builder();
+
+            builder.Hammer = hammer; //Inject dependencies via Setters
+            builder.Saw = saw;//Inject dependencies via Setters
+
+            builder.BuilHouse();
+
+            Console.ReadKey();
+
+        }
+    }
+}

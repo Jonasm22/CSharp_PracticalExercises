73c69f6834138a08e1e0db512493947fee5cb1aa
Jonathan Mendoza <john.2114@hotmail.com>
2024-08-21 21:58:53 +0200

    DateTime Added

---
+++ DateTimeT/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace DateTimeT
+{
+    internal class Program
+    {
+
+        //SMALL EXAMPLE HOW TO MANAGE DATE TIME
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(2018, 5, 21);
+
+            Console.WriteLine($"Entry At: {dateTime}");
+
+            //WRITE TODAY ON SCREEN
+
+            Console.WriteLine(DateTime.Today);
+
+            // Write current time on screen
+            Console.WriteLine(DateTime.Now);
+
+            DateTime tomorrow = getTomorrow();
+            Console.WriteLine("Tomorrow will be: " + getTomorrow());
+            Console.WriteLine("Today is: " + DateTime.Today.DayOfWeek);
+            Console.WriteLine("First day of year is: " + GetFirstDayOfYear(1999));
+
+            int day = DateTime.DaysInMonth(2000, 2);
+            Console.WriteLine("Day in Feb" + day);
+
+
+
+        }
+
+
+            public static DateTime getTomorrow()
+        {
+            return DateTime.Today.AddDays(1);
+        }
+        
+        public static DateTime GetFirstDayOfYear(int year)
+        {
+            return new DateTime(year ,1,1) ;
+        }
+
+
+
+    }
+}
---
+++ EnumsC/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EnumsC
+{
+
+    enum Day { Mon, Tu, We, Th,Fr, Sa, Sun};
+    enum Month { Jan = 1, Fr, Mar,Apr,Jun, Jul = 12,Aug};
+
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Day fr = Day.Fr;
+            Day su = Day.Sun;
+            Day a = Day.Fr;
+             
+
+            Console.WriteLine(fr== a);
+            Console.WriteLine(Day.Mon);
+            Console.WriteLine((int)Day.Mon);
+
+            Console.WriteLine((int)Month.Fr);
+            Console.WriteLine((int)Month.Jul);
+            Console.WriteLine((int)Month.Aug);
+          
+
+
+        }
+    }
+}

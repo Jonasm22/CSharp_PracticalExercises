8bf5eb74e0437b0aea82a51728322a6e8b277f1a
Jonathan Mendoza <john.2114@hotmail.com>
2024-08-21 22:10:22 +0200

    Math Lecture Added

---
+++ MathE/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MathE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Celling: " + Math.Ceiling(15.3));
+            Console.WriteLine("Floor: " + Math.Floor(15.3));
+
+            int num1 = 13;
+            int num2 = 9;
+
+            // Realice Exercise with Min, Max, PI, Pow, Sqrt, cos.
+            // uses some values
+            Console.ReadLine();
+
+        }
+    }
+}
